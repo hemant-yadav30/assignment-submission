37f5abc02d7c0fe9655b7a6ce039abbc3165cfda
Hemant Yadav <99952622+hemant-yadav30@users.noreply.github.com>
2022-02-20 00:12:46 +0530

    day 2 Assignment Submission

---
+++ DAY 2.py
@@ -0,0 +1,46 @@
+# Name the keyword which helps in writing code involving conditions
+# if None:
+
+# Write the syntax of a simple if statement
+
+ravi =10
+if ravi<20:
+    print("Less then")
+else:
+    print("greter then")
+
+# Write a program to check whether a person is eligible for voting or not. (accept age from user)
+
+a = input("enter your name :")
+b = int(input("enter your age : "))
+if b>18:
+    print("your are eligible for voting")
+else:
+    print("your are not eligible for voting")
+print("Thanks you")
+
+# What is the output of the given below program?
+#
+# if 1 + 3 == 7:
+#     print("Hello")
+# else:
+#     print("Know Program")
+
+#   Ans :Know Program
+
+
+# Write a program to check whether a number entered by user is even or odd.
+
+a = int(input("enter  your number :"))
+if (a%2)==0:
+    print("{0} is even".format(a))
+else:
+    print("{0} is odd ".format(0) )
+
+
+# Python program to find the largest element among three Numbers
+# NO Ans
+
+
+# Write a program to accept a number from 1 to 12 and display name of the month and days in that month like 1 for January and number of days 31 and so on
+#  no Ans
