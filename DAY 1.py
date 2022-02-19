bc53b57ffe69cf33b090177ea7738f09afc2d6c9
Hemant Yadav <99952622+hemant-yadav30@users.noreply.github.com>
2022-02-19 23:13:27 +0530

    DAY 1 ASSIGNMENT SUBMISSION

---
+++ DAY 1.py
@@ -0,0 +1,27 @@
+# 1.
+
+print("hello word")
+print("\n")
+
+h = "Hemant_yadav30"
+print(h)
+print("\n")
+
+# 2.
+
+k = 5
+m = 10
+print(k+m)
+print("\b")
+
+ravi = 10
+yadav = 100
+result = ravi + yadav
+print(result)
+
+print("welcome to letupgrate")
+
+
+
+
+# print(dir(str))
